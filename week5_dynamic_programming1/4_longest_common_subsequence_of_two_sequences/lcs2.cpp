7b1955de2d497195306ffa6f51a69c12e454fddb
konmeso <kwstas.meso@gmail.com>
2022-11-30 22:19:31 +0200

    All LCS passed

--- week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
+++ week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
@@ -1,11 +1,43 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
-using std::vector;
+using namespace std;
 
 int lcs2(vector<int> &a, vector<int> &b) {
-  //write your code here
-  return std::min(std::min(a.size(), b.size()), c.size());
+
+  int n = a.size();
+  int m = b.size();
+
+  vector<vector<int>> dTable(n+1,vector<int>(m+1));
+
+  for (size_t i = 0 ; i <= n ; i++){
+    dTable[i][0] = 0;
+  }
+
+  for (size_t j = 0 ; j <= m ; j++){
+    dTable[0][j] = 0;
+  }
+
+  for (size_t i = 1 ; i <= n ; i++){
+    for (size_t j = 1 ; j <= m ; j++){
+      if (a[i-1] == b [j-1]){
+        dTable[i][j] = 1 + dTable[i-1][j-1];
+      }
+      else{
+        dTable[i][j] = max(dTable[i-1][j],dTable[i][j-1]);
+      }
+    }
+  }
+
+  /*
+  for (size_t i = 0 ; i <= n ; i++){
+    for (size_t j = 0 ; j <= m ; j++){
+      cout << dTable[i][j] << " ";
+    }
+    cout << endl;
+  }
+  */
+
+  return dTable[n][m];
 }
 
 int main() {
