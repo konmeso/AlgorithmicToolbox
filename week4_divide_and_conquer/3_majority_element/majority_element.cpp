38a5ba5477a9c252a98ddc0f02e559ea12247a3b
konmeso <kwstas.meso@gmail.com>
2022-12-02 19:52:35 +0200

    Certificate passed.

--- week4_divide_and_conquer/3_majority_element/majority_element.cpp
+++ week4_divide_and_conquer/3_majority_element/majority_element.cpp
@@ -1,80 +1,49 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-#include <cmath>
 
 using std::vector;
 
-long long binary_search(const vector<long long> &a, long long x) {
-  long long left = 0, right = (long long) a.size(); 
-  long long midIdx;
+long get_majority_element(vector<long> &a, int left, int right) {
+	if (left == right) {
+		return a[left]; 
+	}
 
-  while (right >= left){
-    midIdx = floor((left+right)/2);
-    if (a[midIdx] == x){
-      return midIdx;
-    }
-    else if (a[midIdx] < x){
-      left = midIdx + 1;
-    }
-    else{
-      right = midIdx - 1;
-    }
-  }
+	if (left + 1 == right) {
+		if (a[left] != a[right]){
+			return -1;
+			}
+		else
+			return a[left];
+	}
 
-return -1;
+	int mid = left + (right - left) / 2;
+	int count_x = 0, count_y = 0;
 
-}
+	long x = get_majority_element(a, left, mid);
+	long y = get_majority_element(a, mid + 1, right);
 
-int get_majority_element(vector<long long> &a) {
+	for (int i = left; i <= right; i++) {
+		if (x == a.at(i)) {
+			count_x++;
+		} 
+		else if (y == a.at(i)) {
+			count_y++;
+		}
+	}
 
-  long long sizeA = a.size();
-  vector<long long> uniqueArr;
-  vector<long long> scores;
-  long long idx;
+	int n = right - left + 1;
 
-  uniqueArr.push_back(a[0]);
-  scores.push_back(1);
+	if (count_x > count_y && count_x > n / 2) return x;
+	if (count_x < count_y && count_y > n / 2) return y;
 
-  for (size_t ii = 1; ii < sizeA ; ii++){
-    idx = binary_search(uniqueArr,a[ii]);
-    if (idx == -1){
-      uniqueArr.push_back(a[ii]);
-      scores.push_back(1);
-    }
-    else{
-      scores[idx]++;
-    }
-  }
-
-  int max = *max_element(scores.begin(),scores.end());
-
-  if (max > sizeA/2){
-    return 1;
-  }
-  else{
-    return 0;
-  }
-
-}
-
-int get_majority_element(vector<long long> &a, long long left, long long right){
-
-  if (left==right){
-    return a[left];
-  }
-
-  long long mid = floor((left+right)/2);
-
-
+	return -1;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<long long> a(n);
-  for (size_t i = 0; i < a.size(); ++i) {
-    std::cin >> a[i];
-  }
-  std::cout << get_majority_element(a) << '\n';
+	int n;
+	std::cin >> n;
+	vector<long> a(n);
+	for (size_t i = 0; i < a.size(); ++i) { std::cin >> a[i]; }
+	std::cout << (get_majority_element(a, 0, a.size() - 1) != -1) << '\n';
 }
